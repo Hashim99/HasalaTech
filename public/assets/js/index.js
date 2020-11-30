f9a89aa31b4a0656c855f82a575eaefb106d01fa
Hashim Al-Ghamdi <49069675+Hashim99@users.noreply.github.com>
2020-11-30 16:33:08 +0300

    First Commit

---
+++ public/assets/js/index.js
@@ -0,0 +1,53 @@
+$("#income-modal-button").click(function(){
+    $("#incomeModal").modal("show");
+
+});
+
+$("#balance-modal-button").click(function(){
+    $("#balanceModal").modal("show");
+
+});
+
+$("#spendings-modal-button").click(function(){
+    $("#spendingsModal").modal("show");
+
+});
+
+$("#bills-modal-button").click(function(){
+    $("#billsModal").modal("show");
+
+});
+
+$("#categories-modal-button").click(function(){
+    $("#categoriesModal").modal("show");
+
+});
+
+$("#wallet-modal-button").click(function(){
+    $("#addWalletModal").modal("show");
+
+});
+
+$("#bill-modal-button").click(function(){
+    $("#addBillModal").modal("show");
+
+});
+
+
+$("#walletLink").click(function(){
+    $("#walletModal").modal("show");
+
+});
+
+$("#billLink").click(function(){
+    $("#billModal").modal("show");
+
+});
+
+$("#editIncome").click(function(){
+    $("#editIncomeModal").modal("show");
+
+});
+
+
+
